9c9a28a789d23a51acb0d3a7518e5b00a4c7fdc8
André <andrer@ppedv.de>
2019-11-13 13:50:44 +0100

    Hallo WCF

---
+++ WcfServiceLibrary1/WcfServiceLibrary1/IService1.vb
@@ -0,0 +1,24 @@
+﻿<ServiceContract()>
+Public Interface IService1
+
+    <OperationContract()>
+    Function GetData(ByVal value As Integer) As String
+
+    <OperationContract>
+    Function GetWochentag() As String
+
+    <OperationContract>
+    Function GetSchnitzel() As Schnitzel
+
+End Interface
+
+<DataContract>
+Public Class Schnitzel
+
+    <DataMember()>
+    Public Property HatSoße As Boolean
+
+    Public Property Geheim As String
+
+End Class
+
---
+++ WcfServiceLibrary1/WcfServiceLibrary1/Service1.vb
@@ -0,0 +1,18 @@
+﻿Imports WcfServiceLibrary1
+
+Public Class Service1
+    Implements IService1
+
+    Public Function GetData(ByVal value As Integer) As String Implements IService1.GetData
+        Return String.Format("You entered: {0}", value)
+    End Function
+
+    Public Function GetWochentag() As String Implements IService1.GetWochentag
+        Return Date.Now.ToString("dddd")
+    End Function
+
+    Public Function GetSchnitzel() As Schnitzel Implements IService1.GetSchnitzel
+        Return New Schnitzel()
+
+    End Function
+End Class
